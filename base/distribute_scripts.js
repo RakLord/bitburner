dea19a454d212e235ed270b8a46cb67f4de013a7
shaeca16 <48102560+shaeca16@users.noreply.github.com>
2022-01-01 21:24:50 +0000

    Initial upload

---
+++ base/distribute_scripts.js
@@ -0,0 +1,21 @@
+//rak_hak.js
+export async function main(ns) {
+	ns.disableLog("ALL");
+	var serversSeen = ns.scan("home");
+	var currentScan;
+
+	for (let i = 0; i < serversSeen.length; i++) {
+		currentScan = ns.scan(serversSeen[i])
+		for (var j = 0; j < currentScan.length; j++) {
+			if (serversSeen.indexOf(currentScan[j]) === -1) {
+				serversSeen.push(currentScan[j])
+			}
+		}
+	}
+	ns.tprint(serversSeen);
+	for (let i = 0; i < serversSeen.length; i++) {
+		await ns.scp("weak.js", "home", serversSeen[i]);
+		await ns.scp("grow.js", "home", serversSeen[i]);
+		await ns.scp("hack.js", "home", serversSeen[i]);
+	}
+}
