d43c8be7206584e5a11d0f2a1c9b732dc1bf2ff7
shaeca16 <48102560+shaeca16@users.noreply.github.com>
2022-01-02 00:50:20 +0000

    Update primerv2.js

--- primer/primerv2.js
+++ primer/primerv2.js
@@ -17,12 +17,41 @@ export async function main(ns) {
 
 	var primed = false;
 	while (!primed) {
+		var pservCount = serversSeen.length;
 		var availableThreads = Math.floor((ns.getServerMaxRam(serversSeen[0]) - ns.getServerUsedRam(serversSeen[0])) / WGScriptSize);
-		ns.print(`Available Threads: ${availableThreads}`);
-
 		var weakThreads = Math.ceil((ns.getServerSecurityLevel(target) - securityThresh) / 0.05);
+		var growRatio = maxMoney / ns.getServerMoneyAvailable(target);
+		var growThreads = ns.growthAnalyze(target, growRatio);
+		var weakTime = ns.getWeakenTime(target);
+		var growTime = ns.getGrowTime(target);
+		var timeDif = weakTime - growTime - 100;
+		ns.print(`Available threads: ${availableThreads}`);
 		ns.print(`Weak threads: ${weakThreads}`);
+		ns.print(`Growth ratio: ${growRatio}`);
+		ns.print(`Grow threads: ${growThreads}`);
+		ns.print(`Weak time: ${ns.nFormat(weakTime / 1000, "00:00:00")}`);
+		ns.print(`Grow time: ${ns.nFormat(growTime / 1000, "00:00:00")}`);
+		ns.print(`End delay: ${ns.nFormat(timeDif / 1000, "00:00:00")}`);
 
+		var distributedWeakThreads = Math.ceil(weakThreads / pservCount);
+		var distributedGrowThreads = Math.ceil(growThreads / pservCount);
+
+		
+		if (weakThreads > 0) {
+			for (let i = 0; i < pservCount; i++) {
+				var curServ = serversSeen[i];
+				ns.exec("weak.js", curServ, distributedWeakThreads, target);
+			}
+		}
+
+
+		await ns.sleep(timeDif);
+		if (growThreads > 0) {
+			for (let i = 0; i < pservCount; i++) {
+				var curServ = serversSeen[i];
+				ns.exec("grow.js", curServ, distributedGrowThreads, target);
+			}
+		}
 
 
 		
@@ -34,7 +63,6 @@ export async function main(ns) {
 			ns.exit();
 		}
 
-		ns.print(`Priming duration: ${ns.nFormat((ns.getWeakenTime(target) + 30) / 1000 , "00:00:00")}`);
-		await ns.sleep(ns.getWeakenTime(target) + 30);
+		await ns.sleep(1000);
 	} 
 }
