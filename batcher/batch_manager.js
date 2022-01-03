f451edb7a76ae037945e1d3df69b4ccd3b234c09
shaeca16 <48102560+RakLord@users.noreply.github.com>
2022-01-03 01:15:42 +0000

    Update batch_manager.js

--- batcher/batch_manager.js
+++ batcher/batch_manager.js
@@ -2,8 +2,9 @@
 export async function main(ns) {
 	// ns.disableLog("ALL");
 	var target = ns.args[0];
+    const batches = 20; // EDIT THIS VALUE TO CHANGE THE BATCH COUNT ( check pserv ram usage to see if u can use more)
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < batches; i++) {
         ns.tprintf(`SUCCESS Batch started ${i}`);
         ns.run("batcher/singe_batch.js", 1, target, Math.random());
     }
