02a60da6d06a2efcd55a5483a5b99f68ff7b1ec0
shaeca16 <48102560+RakLord@users.noreply.github.com>
2022-01-03 04:48:14 +0000

    Update batch_manager.js

--- batcher/batch_manager.js
+++ batcher/batch_manager.js
@@ -2,17 +2,14 @@
 export async function main(ns) {
 	// ns.disableLog("ALL");
 	var target = ns.args[0];
-    var keepOpen = ns.args[1];
     var batches = 1000;
 
+    var batchLoops = batches / 25 / 4 // batches / pservcount / 4 scripts
+
     ns.tprintf(`Starting ${batches} batches...`)
-    for (let i = 0; i < batches; i++) {
-        ns.tprintf(`SUCCESS Batch started ${i}`);
+    for (let i = 0; i < batchLoops; i++) {
+        ns.tprintf(`SUCCESS Batches started ${i * 25 * 4}`);
         ns.run("batcher/singe_batch.js", 1, target, i);
         await ns.sleep(0); // stop launch lag
-    }
-
-    while (keepOpen) {
-        await ns.sleep(1000000); // keeps batch manager open as such u can see the income in the active scripts tab
     }
 }
